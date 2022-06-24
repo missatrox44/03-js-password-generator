49007b3e10b148a741d19c16d8f19c9b429ab9f8
Sara Baqla <missatrox44@gmail.com>
2022-06-24 18:40:51 -0500

    JS rough draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,21 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword () {
   //prompt = length 
-    //let pwdLength = prompt("How many Characters?");
-    //consoloe.log pwdLength
+  let pwdLength = prompt("Type desired password length", "8 - 128 characters");
+  // console.log(pwdLength);
   //confirm - lowercase, uppercase, numeric, special characters
+  if (confirm("Password length recorded!")); 
+  alert('Your password will include lowercase letters.');
+  alert('Your password will include uppercase letters.');
+  alert('Your password will include special characters.');
+  //   let lowerCase = prompt("Okay to include lower case letters?", "Yes or no?");
+  // } else {
+  //   txt = "Yes";
+  // }
     //let numbers = confirm("how mnay things");
+  
   //create var to hold answer to prompt/confirm
+
   //function to validate user answers
   //if and equality operators
     //if(numbers){
@@ -29,21 +39,28 @@ function generatePassword () {
     // }
   //list of numbers, special chars, uppercase adn lower case
     //array
+    var specialCharacters = ["!", "", "#", "$", "%", "&", " ' ", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    // console.log(specialCharacters);
+    // console.log(lowerCase);
+    // console.log(upperCase);
+    // console.log(numbers);
+
     //combine all arrays
+    var pwdArray = specialCharacters.concat(lowerCase, upperCase, numbers);
+    // console.log(pwdArray);
+
     //random choose characters
+  var randomCharacters = pwdArray[Math.floor(Math.random() * pwdArray.length)];
+  // console.log(randomCharacters);
+
     //create a string set equal to password var
+    
   //generate random password
   //return password
-
-
 
   return password;
 }
 
-  // //Arrays
-  
-  // var specialCharacters = ["!", "", "#", "$", "%", "&", " ' ", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-  // var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  // var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  // var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  
